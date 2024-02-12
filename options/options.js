832e7068508e6ff9c7613eabd262b44986995dff
django <django@40937cdc2480>
2024-02-12 19:06:09 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "red-sky-3",
+  url: "https://red-sky-3.botics.co",
+  api: "https://red-sky-3.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
